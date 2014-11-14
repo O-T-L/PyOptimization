2d0c4d9a63219969aa043244e32234497ccb08ac
申瑞珉 (Ruimin Shen) <ruimin0shen@gmail.com>
2014-11-14 19:52:56 +0800

    update

--- PyOptimization/tools/reformat.py
+++ PyOptimization/tools/reformat.py
@@ -1,12 +1,27 @@
+"""
+Copyright (C) 2014, 申瑞珉 (Ruimin Shen)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import os
 import numpy
 import platform
 import fnmatch
-import sqlite3
 import configparser
-import pyoptimization.utility
 
-def reformat(cursor, table, path, _reformat, delimiter = '\t'):
+def reformat(path, _reformat, delimiter = '\t'):
 	data = numpy.loadtxt(path, dtype = bytes, ndmin = 2).astype(str)
 	pathBase = os.path.splitext(path)[0]
 	for ext, _data in _reformat(data):
@@ -15,14 +30,11 @@ def reformat(cursor, table, path, _reformat, delimiter = '\t'):
 
 def main():
 	config = configparser.ConfigParser()
-	pyoptimization.utility.read_config(config, __file__)
+	config.read(os.path.splitext(__file__)[0] + '.ini')
 	name = os.path.splitext(os.path.basename(__file__))[0]
 	pathRoot = config.get(name, 'root.' + platform.system())
 	pattern = config.get(name, 'pattern')
 	_reformat = eval(config.get(name, 'reformat'))
-	conn = sqlite3.connect(os.path.expandvars(config.get('database', 'file.' + platform.system())))
-	table = config.get('database', 'table')
-	cursor = conn.cursor()
 	paths = []
 	for pathParent, _, fileNames in os.walk(pathRoot):
 		for fileName in fileNames:
@@ -31,9 +43,7 @@ def main():
 				paths.append(path)
 	for path in paths:
 		print(path)
-		reformat(cursor, table, path, _reformat)
-	conn.commit()
-	print('Finished normally')
+		reformat(path, _reformat)
 
 if __name__ == '__main__':
 	main()
