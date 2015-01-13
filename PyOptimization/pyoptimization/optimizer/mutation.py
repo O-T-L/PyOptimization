feafedf5635f94ad6685a3b442f1cf230b0f233f
申瑞珉 (Ruimin Shen) <ruimin0shen@gmail.com>
2015-01-13 11:42:03 +0800

    improvements on operators for a better batch capacity

--- PyOptimization/pyoptimization/optimizer/mutation.py
+++ PyOptimization/pyoptimization/optimizer/mutation.py
@@ -24,68 +24,72 @@ import pyoptimization.problem.coding
 import pyoptimization.optimizer.fetcher.mutation
 
 def get_mutations_real(config, problem, random, coding):
-	probability = eval(config.get(coding + '_mutation', 'probability'))(problem)
 	mutations = []
 	if config.getboolean(coding + '_mutation_switch', 'polynomial_mutation'):
-		distribution_index = config.getfloat('polynomial_mutation', 'distribution_index')
-		mutation = pyotl.mutation.real.PolynomialMutation(random, probability, problem.GetBoundary(), distribution_index)
 		fetcher = pyoptimization.optimizer.fetcher.mutation.pm
-		mutations.append([mutation, fetcher])
+		for probability in eval(config.get(coding + '_mutation', 'probability'))(problem):
+			for distribution_index in map(float, config.get('polynomial_mutation', 'distribution_index').split()):
+				mutation = pyotl.mutation.real.PolynomialMutation(random, probability, problem.GetBoundary(), distribution_index)
+				mutations.append([mutation, fetcher])
 	return mutations
 
 def get_mutations_integer(config, problem, random, coding):
-	probability = eval(config.get(coding + '_mutation', 'probability'))(problem)
 	mutations = []
 	if config.getboolean(coding + '_mutation_switch', 'bitwise_mutation'):
-		mutation = pyotl.mutation.integer.BitwiseMutation(random, probability, problem.GetDecisionBits())
 		fetcher = pyoptimization.optimizer.fetcher.mutation.std
-		mutations.append([mutation, fetcher])
+		for probability in eval(config.get(coding + '_mutation', 'probability'))(problem):
+			mutation = pyotl.mutation.integer.BitwiseMutation(random, probability, problem.GetDecisionBits())
+			mutations.append([mutation, fetcher])
 	return mutations
 
 def get_mutations_dynamic_bitset(config, problem, random, coding):
-	probability = eval(config.get(coding + '_mutation', 'probability'))(problem)
 	mutations = []
 	if config.getboolean(coding + '_mutation_switch', 'bitwise_mutation'):
-		mutation = pyotl.mutation.dynamic_bitset.BitsetBitwiseMutation(random, probability)
 		fetcher = pyoptimization.optimizer.fetcher.mutation.std
-		mutations.append([mutation, fetcher])
+		for probability in eval(config.get(coding + '_mutation', 'probability'))(problem):
+			mutation = pyotl.mutation.dynamic_bitset.BitsetBitwiseMutation(random, probability)
+			mutations.append([mutation, fetcher])
 	return mutations
 
 def get_mutations_index(config, problem, random, coding):
-	probability = eval(config.get(coding + '_mutation', 'probability'))(problem)
 	mutations = []
 	if config.getboolean(coding + '_mutation_switch', 'bitwise_mutation'):
-		decisions = len(problem.GetBoundary())
-		bits = math.ceil(math.log2(decisions))
-		_bits = pyotl.utility.PyList2Vector_size_t([bits] * decisions)
-		mutation = pyotl.mutation.index.BitwiseMutation(random, probability, _bits)
 		fetcher = pyoptimization.optimizer.fetcher.mutation.std
-		mutations.append([mutation, fetcher])
+		for probability in eval(config.get(coding + '_mutation', 'probability'))(problem):
+			decisions = len(problem.GetBoundary())
+			bits = math.ceil(math.log2(decisions))
+			_bits = pyotl.utility.PyList2Vector_size_t([bits] * decisions)
+			mutation = pyotl.mutation.index.BitwiseMutation(random, probability, _bits)
+			mutations.append([mutation, fetcher])
 	return mutations
 
 def get_mutations_tsp(config, problem, random):
-	probability = eval(config.get('tsp_mutation', 'probability'))(problem)
 	mutations = []
 	if config.getboolean('tsp_mutation_switch', 'displacement_mutation'):
-		mutation = pyotl.mutation.index.DisplacementMutation(random, probability)
 		fetcher = pyoptimization.optimizer.fetcher.mutation.std
-		mutations.append([mutation, fetcher])
+		for probability in eval(config.get('tsp_mutation', 'probability'))(problem):
+			mutation = pyotl.mutation.index.DisplacementMutation(random, probability)
+			mutations.append([mutation, fetcher])
 	elif config.getboolean('tsp_mutation_switch', 'exchange_mutation'):
-		mutation = pyotl.mutation.index.ExchangeMutation(random, probability)
 		fetcher = pyoptimization.optimizer.fetcher.mutation.std
-		mutations.append([mutation, fetcher])
+		for probability in eval(config.get('tsp_mutation', 'probability'))(problem):
+			mutation = pyotl.mutation.index.ExchangeMutation(random, probability)
+			mutations.append([mutation, fetcher])
 	elif config.getboolean('tsp_mutation_switch', 'insertion_mutation'):
-		mutation = pyotl.mutation.index.InsertionMutation(random, probability)
 		fetcher = pyoptimization.optimizer.fetcher.mutation.std
-		mutations.append([mutation, fetcher])
+		for probability in eval(config.get('tsp_mutation', 'probability'))(problem):
+			mutation = pyotl.mutation.index.InsertionMutation(random, probability)
+			mutations.append([mutation, fetcher])
 	elif config.getboolean('tsp_mutation_switch', 'inversion_mutation'):
-		mutation = pyotl.mutation.index.InversionMutation(random, probability)
 		fetcher = pyoptimization.optimizer.fetcher.mutation.std
-		mutations.append([mutation, fetcher])
+		for probability in eval(config.get('tsp_mutation', 'probability'))(problem):
+			mutation = pyotl.mutation.index.InversionMutation(random, probability)
+			mutations.append([mutation, fetcher])
 	elif config.getboolean('tsp_mutation_switch', 'spread_mutation'):
-		mutation = pyotl.mutation.index.SpreadMutation(random, probability)
 		fetcher = pyoptimization.optimizer.fetcher.mutation.std
-		mutations.append([mutation, fetcher])
+		for probability in eval(config.get('tsp_mutation', 'probability'))(problem):
+			mutation = pyotl.mutation.index.SpreadMutation(random, probability)
+			mutations.append([mutation, fetcher])
 	return mutations
 
 def get_mutations(config, problem, random):
